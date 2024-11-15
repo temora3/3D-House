70881b02f80f668ca5dac0ec2a53df6379a83071
Charlene393 <131701102+Charlene393@users.noreply.github.com>
2024-11-15 14:04:05 +0300

    door handle and roof side function

--- house.py
+++ house.py
@@ -28,3 +28,53 @@ context.stroke_preserve()
 
 set_rgb_color(context, 216, 165, 141)
 context.fill()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Door handle
+context.arc(540, 300, 5, 0, 2 * 3.14159)  # x=500, y=300, radius=10
+context.set_source_rgb(1, 1, 0)  # Fill color: yellow
+context.fill_preserve()
+context.set_source_rgb(0, 0, 0)  # Stroke color: black
+context.stroke()
+
+#  Roof Side
+context.move_to(90, 200)
+context.line_to(200, 80)
+context.line_to(205, 78)
+context.line_to(295, 260)
+context.line_to(290, 270)
+context.line_to(200, 90)
+context.line_to(90, 210)
+
+context.set_source_rgb(0, 0, 0)  # Black for stroke
+context.set_line_width(2)
+context.stroke_preserve()
+
+set_rgb_color(context, 185, 48, 17)  # Fill color
+context.fill()
+
+context.move_to(295, 260)
+context.line_to(612, 182)
+context.set_source_rgb(0, 0, 0)  # Black for stroke
+context.set_line_width(2)
+context.stroke_preserve()
+

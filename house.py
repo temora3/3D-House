ed9207f90e23358f947829ec4476d342ae6e702b
John Doe <annastacia.nzoka@strathmore.edu>
2024-11-15 14:18:25 +0300

    edwin's code

--- house.py
+++ house.py
@@ -1,3 +1,4 @@
+
 import cairo
 
 WIDTH, HEIGHT = 700, 500
@@ -10,7 +11,7 @@ surface = cairo.ImageSurface(cairo.FORMAT_RGB30, WIDTH, HEIGHT)
 context = cairo.Context(surface)
 
 # Background Colour
-context.set_source_rgb(1, 1, 1)  #White
+context.set_source_rgb(1, 1, 1)  # White
 context.paint()
 
 
@@ -60,4 +61,37 @@ context.set_line_width(4)
 context.stroke_preserve()
 
 set_rgb_color(context, 216, 165, 141)  # Fill color
+context.fill()
+# Door
+context.move_to(500, 370)
+context.line_to(500, 250)
+context.line_to(560, 230)
+context.line_to(560, 350)
+
+context.close_path()
+
+context.set_source_rgb(0, 0, 0)  # Black for stroke
+context.set_line_width(4)
+context.stroke_preserve()
+
+set_rgb_color(context, 164, 106, 85)  # Fill color
+context.fill()
+
+
+# Front Wall
+context.move_to(270, 450)
+context.line_to(500, 380)
+context.line_to(500, 250)
+context.line_to(560, 230)
+context.line_to(560, 360)
+context.line_to(600, 345)
+context.line_to(600, 190)
+context.line_to(272, 240)
+context.close_path()
+
+context.set_source_rgb(0, 0, 0)  # Black for stroke
+context.set_line_width(4)
+context.stroke_preserve()
+
+set_rgb_color(context, 255, 230, 202)  # Fill color
 context.fill()
